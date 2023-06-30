f278a7360e76410e12dab42c083e17c0d30ac21d
Yuliia Liubenka <yuliialiubenka@gmail.com>
2023-06-30 18:52:33 +0300

    Desktop
    Desktop

---
+++ src/main.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    // Tabs
+
+    const tabs = document.querySelectorAll(".tablink");
+    const contents = document.querySelectorAll(".tabcontent");
+
+    for (let i = 0; i < tabs.length; i++) {
+        tabs[i].addEventListener("click", ( event ) => {
+    
+            let tabsChildren = event.target.parentElement.children;
+            for (let t = 0; t < tabsChildren.length; t++) {
+                tabsChildren[t].classList.remove("tab--active");
+            }
+            tabs[i].classList.add("tab--active");
+            let tabContentChildren = event.target.parentElement.nextElementSibling.children;
+            for (let c = 0; c < tabContentChildren.length; c++) {
+                tabContentChildren[c].classList.remove("content--active");
+            }
+            contents[i].classList.add("content--active");  
+        });
+    }
+
+});
